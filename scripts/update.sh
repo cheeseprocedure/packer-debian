30f344315d5bb61d36499c3cc224308dc051f134
Matt Mercer <mgordonmercer+github@gmail.com>
2017-10-10 09:58:41 -0700

    Remove some vbox-specific actions; set up cloud-init

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt-get update
 # install curl to fix broken wget while retrieving content from secured sites
-apt-get -y install curl
+apt-get -y --no-install-recommends install curl cloud-init
+
+echo "datasource_list: [ NoCloud, ConfigDrive, None ]" \
+	> /etc/cloud/cloud.cfg.d/90_dpkg.cfg
